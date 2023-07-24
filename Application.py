f7786763d94c0856d04ec78d2c3bdf3c8044fd77
codderdreamer <sevdayucedagg@gmail.com>
2023-07-24 15:17:44 +0300

    dene

--- Application.py
+++ Application.py
@@ -19,7 +19,8 @@ class Application():
         self.flask_module = FlaskModule(__name__)
         self.flask_module.run(BootError)
 
-        dene = os.system("nmcli dev wifi > home/pi/upwork/network.txt")
+        os.system('pwd')
+        dene = os.system("nmcli dev wifi > network.txt")
         print(dene)
 
        
