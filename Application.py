a16681b49f6e11f196dd3d3f7159c6aacc163cf9
codderdreamer <sevdayucedagg@gmail.com>
2023-07-24 10:26:15 +0300

    dene

--- Application.py
+++ Application.py
@@ -3,6 +3,7 @@ from Modules.FlaskModule import *
 import time
 import subprocess
 import keyboard
+import os
 
 BootError=False
 
@@ -17,6 +18,14 @@ class Application():
         # Flaskı Başlat
         self.flask_module = FlaskModule(__name__)
         self.flask_module.run(BootError)
+
+        # scanning the available Wi-Fi networks  
+        os.system('cmd /c "netsh wlan show networks"')  
+        # providing the Wi-Fi name as input  
+        router_name = input('Input Name/SSID of the Wi-Fi network we would like to connect: ')  
+        # connecting to the provided Wi-Fi network  
+        os.system(f'''cmd /c "netsh wlan connect name = {router_name}"''')  
+        print("If the system is not connected yet, try reconnecting to an earlier connected SSID!")  
 
         # using the check_output() for having the network term retrieval
         try:
