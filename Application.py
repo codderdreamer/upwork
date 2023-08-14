15df534b71490fe29c97aee45f5383554c76aa08
codderdreamer <sevdayucedagg@gmail.com>
2023-08-14 18:52:37 +0300

    dene

--- Application.py
+++ Application.py
@@ -55,6 +55,7 @@ class Application():
             try:
                 time_start = time.time()
                 event = keyboard.read_event()  # Capture a keyboard event
+                print(event)
                 # print(f"{event.name} key was pressed")
                 # print(time_start-time_finish)
                 time_finish = time.time()
