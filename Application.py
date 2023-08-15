f1f60267a1e1be1603060e766b3a787e1d442e19
codderdreamer <sevdayucedagg@gmail.com>
2023-08-15 10:37:37 +0300

    dene

--- Application.py
+++ Application.py
@@ -47,6 +47,7 @@ class Application():
 
 
     def key1(self):
+        print('************************* ctrl+shift+1',"self.key_down_press",self.key_down_press,"self.counter",self.counter)
         if self.key_counter():
             print('************************* ctrl+shift+1')
 
